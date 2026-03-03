af45c3d2a56bce9b0c949cbb98afcba29be42dc1
tsgcode1122 <tsgcode1122@gmail.com>
2026-03-03 13:49:42 -0500

    r

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5009;
 
 // 1. CORS CONFIGURATION (Must be before routes)
 const corsOptions = {
-  origin: ["https://gemluxeaesthetics.com", "http://localhost:3000"], 
+  origin: ["https://gemluxeaesthetics.com", "http://localhost:5173"], 
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, 
